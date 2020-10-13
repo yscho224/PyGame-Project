dbe12415e956a1bd7f927c58c59c5d5367add443
Eric <yscho224@gmail.com>
2020-10-13 20:24:49 +0900

    solve error

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,7 +19,7 @@ class AlienInvasion:
         )
 
         self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().screen_width
+        self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         # giving argument of an instance of Alien invasion to Ship()
