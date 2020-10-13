3ef0dff0cf0bfedd837a5fc56efd1728b924c2d2
Eric <yscho224@gmail.com>
2020-10-13 20:05:02 +0900

    refactoring _check_events()

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,17 +39,22 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN: #KEYDOWN = pressing the key
-                if event.key == pygame.K_RIGHT:
-                    #Move the ship to the right.
-                    #moves the image(rectangle) on the screen
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True  
+                self._check_keydown_events(event)
+                 
             elif event.type == pygame.KEYUP: #KEYUP = releasing the key
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            #Move the ship to the right.
+            #moves the image(rectangle) on the screen
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
                 
     def _update_screen(self):              
         self.screen.fill(self.settings.bg_color)
