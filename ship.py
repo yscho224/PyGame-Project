1bcb0583ba96e77ce94c3fc069f1d011409422b9
Eric <yscho224@gmail.com>
2020-10-13 19:24:15 +0900

    allowing continuous movement

--- ship.py
+++ ship.py
@@ -12,6 +12,13 @@ class Ship:
         self.rect = self.image.get_rect()
         #start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #Movement flag
+        self.moving_right = False
+    def update(self):
+        #update the ship's position based on the movement flag
+        if self.moving_right:
+            self.rect.x += 1
     def blitme(self):
         # Draw the ship at its current location
         # blitme () does draws the image to the screen specified to self.rect
