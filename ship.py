3af0ba59348e63ddb956d2aa4c30bf21a29370bc
Eric <yscho224@gmail.com>
2020-10-13 20:08:42 +0900

    get rid of error

--- ship.py
+++ ship.py
@@ -23,7 +23,7 @@ class Ship:
         #update the ship's x values, not the rect. 
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left and self.rect.left > 0
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # update rect object from self.x.
