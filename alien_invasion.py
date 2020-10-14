23226c8c0d756d31337b7770f7d915ef5a1bfff3
Eric <yscho224@gmail.com>
2020-10-14 17:19:38 +0900

    Detecting bullet collisions

--- alien_invasion.py
+++ alien_invasion.py
@@ -133,6 +133,14 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        
+        #Check for any bullets that have hit aliens
+        #If so, get rid of the bullet and the alien.
+        #Compares the positions of all the bullets and aliens
+        #and identify any that overlap. 
+        # **groupcollide() adds a key-value pair to the dictionary it returns
+        collisions = pygame.sprite.groupcollide(
+                    self.bullets, self.aliens, True, True)# two true arguments tell to delete the bullets and aliens that have collided.
 
     def _update_aliens(self):
         '''Update the positions of all aliens in the fleet'''
