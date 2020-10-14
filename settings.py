d5debd56bab5311cedfa630dce5e376239f503a4
Eric <yscho224@gmail.com>
2020-10-14 17:25:19 +0900

    speeding up the bullets

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ class Settings:
         self.ship_speed = 1.5 #1.5 pixels rather than 1 pixel
 
         #Bullet Settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60) #gray
