56ef081d798262929c95b7a7b0c1206b948707bf
Eric <yscho224@gmail.com>
2020-10-15 20:09:12 +0900

    Resetting speed

--- alien_invasion.py
+++ alien_invasion.py
@@ -131,6 +131,7 @@ class AlienInvasion:
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
             #Reset the game statistics.
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             pygame.mouse.set_visible(False)
     def _check_keydown_events(self, event):
