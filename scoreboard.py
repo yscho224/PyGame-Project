ccda23665acfce30da3bf0a59568913d1d3a37f2
Eric <yscho224@gmail.com>
2020-10-15 21:27:07 +0900

    Rounding the score

--- scoreboard.py
+++ scoreboard.py
@@ -18,7 +18,9 @@ class Scoreboard:
 
     def prep_score(self):
         '''turn the score into a rendered image'''
-        score_str = str(self.stats.score)
+        #round to the nearest 10
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True,
                             self.text_color, self.settings.bg_color)
         #display the score at the top right of the screen
